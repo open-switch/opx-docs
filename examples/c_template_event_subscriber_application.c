d29866234c263c963971eacedea4980a39738a3b
Amy Buck <amy_buck@dell.com>
2017-07-11 12:43:45 -0700

    Update c_template_event_subscriber_application.c
    edits to code

--- examples/c_template_event_subscriber_application.c
+++ examples/c_template_event_subscriber_application.c
@@ -1,9 +1,8 @@
 /****************************************************************** 
-Template   to   perform   a CPS   API   SET
+Template to perform a CPS API SET
 
-This   function   is a sample   of   how   to   compose   a CPS   API   get   request object,   
-and how   to   extract   data   from   the   GET  response.
-
+Sample of how to compose a CPS API GET request object,   
+and how to extract data from the GET  response.
 *******************************************************************/
 
 cps_api_return_code_t   event_publish(cps_api_object_t   event_obj)
@@ -14,7 +13,7 @@ cps_api_return_code_t   event_publish(cps_api_object_t   event_obj)
 
   if (!init_flag)   {
     /*   Not   initialized
-	 =>   Connect   to   CPS   event   subsystem
+	 =>   Connect to CPS event subsystem
     */
 
 
@@ -29,7 +28,7 @@ cps_api_return_code_t   event_publish(cps_api_object_t   event_obj)
       return   (cps_api_ret_code_ERR);
     }
 
-    /*   Mark   as initialized   */
+    /*   Mark as initialized   */
 
     init_flag   =  true;
   }
@@ -53,11 +52,9 @@ cps_api_return_code_t   event_publish(cps_api_object_t   event_obj)
   return   (result);
 }
 
-
-
-
-Python Template: CPS Event Publisher Application
-
+/****************************************************************** 
+Python Template: CPS event publisher application
+******************************************************************/
 
 import   cps
 import   cps_utils
@@ -72,50 +69,47 @@ handle   =  cps.event_connect()
 
 
 
+/****************************************************************** 
+C Template: Event Subscriber Application
 
-  C Template: Event Subscriber Application
-
-  This section describes the structure of a CPS event subscriber implemented in C. It illustrates the initialization of the event service and event processing thread, registration of the event handler function and event processing callback. The key list specified in the registration is used to determine the events that are delivered to this application (in this case, the list contains a single element).
-
+This template illustrates the initialization of the event service 
+and event processing thread, registration of the event handler 
+function and event processing callback. The key list specified 
+in the registration is used to determine the events that are 
+delivered to this application (in this case, the list contains 
+a single element).
+******************************************************************/
 
   bool   event_handler(cps_api_object_t   object,   void   *context)
-{           /*   Extract   key and attributes   of   received   object   */
-
-  /*   Do  something   with   that   information   */
+{           
+  /*   Extract key and attributes of received object   */
+  /*   Do something with that information   */
 }
   
-
 cps_api_return_code_t   event_subscribe()
-{           /*   Connect   to   the   CPS   API   event   service   */
-
+{           
+    /*   Connect to CPS API event service   */
 
     if (cps_api_event_service_init()   !=   cps_api_ret_code_OK)   {
       return   (cps_api_ret_code_ERR);
     } 
 
-
-
-
     if (cps_api_event_thread_init()   !=   cps_api_ret_code_OK)   {
       return   (cps_api_ret_code_ERR);
     }
 
-
-    /*   Register   the   event   handler   function   */
-    cps_api_key_t   key; cps_api_key_init(&key,    ); cps_api_event_reg_t   reg; reg.objects                            =  key;
+    /*   Register event handler function   */
+    cps_api_key_t   key; cps_api_key_init(&key,    ); cps_api_event_reg_t   reg; reg.objects  =  key;
     reg.number_of_objects   =  1;
-
 
     if (cps_api_event_thread_reg(&reg,   event_handler,   0)
 	!=   cps_api_ret_code_OK
 	) {
-      /*   Failed   to   register   handler   */
-
+	    
+      /*   Failed to register handler   */
       return   (cps_api_ret_code_ERR);
     }
 
-    /*   Indicate   success */
-
+    /*   Indicate success */
     return   (cps_api_reg_code_OK);
 }
-
