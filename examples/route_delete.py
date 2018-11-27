deaa37793d01c185ea76b9d428018fba7b51a915
Amy Buck <amy_buck@dell.com>
2018-11-27 14:01:55 -0800

    Update route_delete.py

--- examples/route_delete.py
+++ examples/route_delete.py
@@ -1,9 +1,10 @@
-#Python   code block   to   delete   a route
+#Python code block to delete a route
 
 import   cps_utils
 import   socket
 import   netaddr   as net
 
+# Populate the attributes
 version   =  'ipv4'
 route_ip   =  '70.5.5.0'
 obj   =  cps_utils.CPSObject('base-route/obj/entry')
@@ -25,9 +26,9 @@ print   obj.get()
 cps_update   =  ('delete',   obj.get())
 transaction   =  cps_utils.CPSTransaction([cps_update])
 
+# Commit and check for failure
 ret   =  transaction.commit()
-
-
 if not   ret:
     raise   RuntimeError   ("Error   deleting   Route")
 
+print "Successfully deleted"
