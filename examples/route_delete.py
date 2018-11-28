57f842b16d708664e93803ca18d2836e5f8d6be4
Amy Buck <amy_buck@dell.com>
2018-11-28 09:12:20 -0800

    Update route_delete.py

--- examples/route_delete.py
+++ examples/route_delete.py
@@ -1,34 +1,38 @@
-#Python code block to delete a route
+# Python code block to delete a route
 
-import   cps_utils
-import   socket
-import   netaddr   as net
+import cps_utils
+import socket
+import netaddr as net
 
 # Populate the attributes
-version   =  'ipv4'
-route_ip   =  '70.5.5.0'
-obj   =  cps_utils.CPSObject('base-route/obj/entry')
-obj.add_attr("vrf-id",   0)
 
+version = 'ipv4'
+route_ip = '70.5.5.0'
+obj = cps_utils.CPSObject('base-route/obj/entry')
+obj.add_attr('vrf-id', 0)
 
-if version   ==   'ipv4':
-    obj.add_attr("af",   socket.AF_INET)
-elif   version   ==   'ipv6':
-    obj.add_attr("af",   socket.AF_INET6)
+if version == 'ipv4':
+    obj.add_attr('af', socket.AF_INET)
+elif version == 'ipv6':
+    obj.add_attr('af', socket.AF_INET6)
 
-ip   =  net.IPNetwork(route_ip) 
+ip = net.IPNetwork(route_ip)
 
-obj.add_attr_type("route-prefix",   version)
-obj.add_attr("route-prefix",   str(ip.network))
-obj.add_attr("prefix-len",   int(ip.prefixlen))
+obj.add_attr_type('route-prefix', version)
+obj.add_attr('route-prefix', str(ip.network))
+obj.add_attr('prefix-len', int(ip.prefixlen))
 
-print   obj.get()
-cps_update   =  ('delete',   obj.get())
-transaction   =  cps_utils.CPSTransaction([cps_update])
+print obj.get()
+cps_update = ('delete', obj.get())
+transaction = cps_utils.CPSTransaction([cps_update])
 
-# Commit and check for failure
-ret   =  transaction.commit()
-if not   ret:
-    raise   RuntimeError   ("Error   deleting   Route")
+# Commit transaction
 
-print "Successfully deleted"
+ret = transaction.commit()
+
+# Check for failure
+
+if not ret:
+    raise RuntimeError('Error   deleting   Route')
+
+print 'Successfully deleted'
