45c303c2c912f32b8b654ffebd8100a9f076c840
Amy Buck <amy_buck@dell.com>
2018-11-28 09:23:08 -0800

    Update create_acl_table.py
    line 28 has an invalid syntax

--- examples/create_acl_table.py
+++ examples/create_acl_table.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 """
-Simple   Base   ACL  CPS   config   using   the   generic   CPS   Python   module   and utilities.
-Create   ACL  Table
-Create   ACL  Entry   to   Drop   all   packets   received   on  specific   port   from   specific   Src   MAC """
+# Simple   Base   ACL  CPS   config   using   the   generic   CPS   Python   module   and utilities.
+# Create   ACL  Table
+# Create   ACL  Entry   to   Drop   all   packets   received   on  specific   port   from   specific   Src   MAC """
 
 import   cps_utils
 import   nas_os_utils
@@ -13,7 +13,6 @@ e_ftype   =  {'SRC_MAC':   3,   'DST_MAC':   4,   'SRC_IP':   5,   'DST_IP':   6
 e_atype   =  {'PACKET_ACTION':   3,   'SET_TC':   10}
 e_ptype   =  {'DROP':   1}
 
-
 #  Inform   CPS   utility   about   the   type   of   each attribute
 type_map   =  {
    'base-acl/entry/SRC_MAC_VALUE/addr':   'mac', 
@@ -22,7 +21,6 @@ type_map   =  {
 
 for   key,val   in   type_map.items():
     cps_utils.cps_attr_types_map.add_type(key,   val)
-
 
 #  Create   ACL  Table
 #
@@ -33,6 +31,7 @@ cps_obj.add_list   ('allowed-match-fields',   [e_ftype['SRC_MAC'], e_ftype['DST_
 
 #  Associate   the   CPS   Object   with   a CPS   operation
 cps_update   =  ('create',   cps_obj.get())
+
 #  Add   the   CPS   object   to   a new   CPS   Transaction
 cps_trans   =  cps_utils.CPSTransaction([cps_update])
 
@@ -44,7 +43,6 @@ ret   =  cps_utils.CPSObject   (module='base-acl/table',   obj=r[0]['change'])
 tbl_id   =  ret.get_attr_data   ('id')
 print   "Successfully   created   ACL  Table   " +  str(tbl_id)
 
-
 #  Create   ACL  entry
 #  Drop   all   packets   received   on  specific   port   from   specific   range   of   MACs
 #
@@ -53,20 +51,19 @@ cps_obj   =  cps_utils.CPSObject(module='base-acl/entry')
 cps_obj.add_attr   ('table-id',   tbl_id)
 cps_obj.add_attr   ('priority',   512)
 
-
 #  Filters     #  Match   Filter   1  - Src   MAC
 cps_obj.add_embed_attr   (['match','0','type'],   e_ftype['SRC_MAC'])
+
 #  The 2  at   the   end indicates   that   the   type   should   be deducted   from   the   last   2  attrs   (SRC_MAC_VALUE,addr)
 cps_obj.add_embed_attr   (['match','0','SRC_MAC_VALUE','addr'], '50:10:6e:00:00:00',   2)
+
 #  Match   Filter   2  - Rx  Port
 cps_obj.add_embed_attr   (['match','1','type'],   e_ftype['IN_PORT'])
 cps_obj.add_embed_attr   (['match','1','IN_PORT_VALUE'],   nas_os_utils.if_nametoindex('e101-001-0')) 
 
-
 #  Action   - Drop
 cps_obj.add_embed_attr   (['action','0','type'],   e_atype['PACKET_ACTION'])
 cps_obj.add_embed_attr   (['action','0','PACKET_ACTION_VALUE'],   e_ptype['DROP'])
-
 
 #  Associate   the   CPS   Object   with   a CPS   operation cps_update   =  ('create',   cps_obj.get())
 #  Add   the   CPS   object   to   a new   CPS   Transaction cps_trans   =  cps_utils.CPSTransaction([cps_update])
@@ -75,8 +72,8 @@ cps_obj.add_embed_attr   (['action','0','PACKET_ACTION_VALUE'],   e_ptype['DROP'
 if not   r:
     raise   RuntimeError   ("Error   creating   MAC   ACL  Entry")
 
+# Check for failure
 ret   =  cps_utils.CPSObject   (module='base-acl/entry',   obj=r[0]['change'])
 mac_eid   =  ret.get_attr_data   ('id')
+
 print   "Successfully   created   MAC   ACL  Entry   " +  str(mac_eid)
-
-
