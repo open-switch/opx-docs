70f2f6c6e7dfc58b51f4f61193b2cdad1f2516d0
Amy Buck <amy_buck@dell.com>
2018-11-28 10:04:03 -0800

    Update publish_events.py

--- examples/publish_events.py
+++ examples/publish_events.py
@@ -1,11 +1,18 @@
-import   cps
-import   cps_utils
+#!/usr/bin/python
+# Python code to publish events
 
+import cps
+import cps_utils
 
-handle   =  cps.event_connect()
+# Create a handle to connect to the event service
 
+handle = cps.event_connect()
 
-obj   =  cps_utils.CPSObject('base-port/interface',qual='observed', data=   {"ifindex":23})
+# Create a CPS object
 
-cps.event_send(handle,   obj.get()) 
+obj = cps_utils.CPSObject('base-port/interface', qual='observed',
+                          data={'ifindex': 23})
 
+# Publish the CPS object
+
+cps.event_send(handle, obj.get())
