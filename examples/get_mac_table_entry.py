7038231a37d805de49e744fa242c1ee85ac65157
Amy Buck <amy_buck@dell.com>
2018-11-28 09:53:28 -0800

    Update get_mac_table_entry.py
    Invalid syntax on line 21

--- examples/get_mac_table_entry.py
+++ examples/get_mac_table_entry.py
@@ -1,12 +1,26 @@
-import   cps_utils import   cps
+#!/usr/bin/python
+# Python code to request MAC address table entry
+
+import   cps_utils 
+import   cps
+
+# Register the attribute type
 
 cps_utils.add_attr_type("base-mac/query/mac-address",   "mac")
 
+# Define the MAC address request type
 
 d  =     {"mac-address":   "00:0a:0b:cc:0d:0e","request-type":"2"}
+
+# Associate the get operation with the CPS object
+
 obj   =  cps_utils.CPSObject('base-mac/query', data=   d)
 
+# Create an object filter list
+
 filter_list   =  [] filter_list.append(obj.get()) l =  []
+
+# Check for failure
 
 if cps.get(filter_list,l):
     if l:
@@ -17,4 +31,3 @@ if cps.get(filter_list,l):
 else:
     print   "No   objects   found" 
     raise   RuntimeError   ("Error   Getting   MAC   Table   Entries")
-
