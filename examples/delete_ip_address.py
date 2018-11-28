59425d719ad8350b71d901dfe4a507f02e5e6eb9
Amy Buck <amy_buck@dell.com>
2018-11-28 09:08:51 -0800

    Update delete_ip_address.py

--- examples/delete_ip_address.py
+++ examples/delete_ip_address.py
@@ -1,27 +1,33 @@
-#Python code block to delete IP address
+# Python code block to delete IP address
 
-import   cps_utils
+import cps_utils
 
 # Populate attributes for the CPS object
 
-idx=16
-ip_addr="10.0.0.1"
-pfix_len=16
-ip_attributes   =  {"base-ip/ipv4/ifindex":   idx,"ip":ip_addr,"prefix-length":pfix_len}
+idx = 16
+ip_addr = '10.0.0.1'
+pfix_len = 16
+ip_attributes = {'base-ip/ipv4/ifindex': idx, 'ip': ip_addr,
+                 'prefix-length': pfix_len}
 
 # Create CPS object
-cps_utils.add_attr_type('base-ip/ipv4/address/ip',"ipv4")
-cps_obj=cps_utils.CPSObject('base-ip/ipv4/address',data=ip_attributes)
+
+cps_utils.add_attr_type('base-ip/ipv4/address/ip', 'ipv4')
+cps_obj = cps_utils.CPSObject('base-ip/ipv4/address',
+                              data=ip_attributes)
 
 # Create CPS transaction to delete the CPS object
-cps_update   =  ('delete',   cps_obj.get())
-transaction   =  cps_utils.CPSTransaction([cps_update])
+
+cps_update = ('delete', cps_obj.get())
+transaction = cps_utils.CPSTransaction([cps_update])
 
 # Commit the transaction
-ret   =  transaction.commit()
+
+ret = transaction.commit()
 
 # Check for failure
-if not   ret:
-    raise   RuntimeError   ("Error   ")
 
-print "Successfully deleted"
+if not ret:
+    raise RuntimeError('Error   ')
+
+print 'Successfully deleted'
