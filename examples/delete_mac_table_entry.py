0c8b86c0823455bab06ee9573ad56384067bd0a3
Amy Buck <amy_buck@dell.com>
2018-11-28 09:56:50 -0800

    Update delete_mac_table_entry.py

--- examples/delete_mac_table_entry.py
+++ examples/delete_mac_table_entry.py
@@ -1,14 +1,32 @@
-import   cps_utils
+#!/usr/bin/python
+# Python code to delete MAC address table entry
 
-cps_utils.add_attr_type("base-mac/table/mac-address",   "mac")
+import cps_utils
 
-d      =  {"mac-address":   "00:0a:0b:cc:0d:0e", "ifindex":   18, "vlan":   "100"}
-obj    =  cps_utils.CPSObject('base-mac/table',data=d)
-tr_obj =  ('delete',   obj.get())
+# Register the attribute type to convert between string and byte-array format
+
+cps_utils.add_attr_type('base-mac/table/mac-address', 'mac')
+
+# Define the MAC address interface index, and VLAN to delete the static address entry
+
+d = {'mac-address': '00:0a:0b:cc:0d:0e', 'ifindex': 18, 'vlan': '100'}
+
+# Create a CPS object
+
+obj = cps_utils.CPSObject('base-mac/table', data=d)
+
+# Add the operation to the CPS object
+
+tr_obj = ('delete', obj.get())
+
+# Create a transaction object
 
 transaction = cps_utils.CPSTransaction([tr_obj])
 
-ret = transaction.commit()
-if not   ret:
-    raise   RuntimeError("Error   deleting   entry   from   MAC   Table")
+# Check for failure
 
+ret = transaction.commit()
+if not ret:
+    raise RuntimeError('Error   deleting   entry   from   MAC   Table')
+
+print 'Successfully deleted'
