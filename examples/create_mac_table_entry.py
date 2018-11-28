3e2bcb68854aae5042ff23066ac5bae7cf8e873f
Amy Buck <amy_buck@dell.com>
2018-11-28 09:47:31 -0800

    Update create_mac_table_entry.py

--- examples/create_mac_table_entry.py
+++ examples/create_mac_table_entry.py
@@ -1,16 +1,33 @@
-### Configure MAC address table entry
-import   cps_utils
+#!/usr/bin/python
+# Configure MAC address table entry
 
-cps_utils.add_attr_type("base-mac/table/mac-address",   "mac")
+import cps_utils
 
-d  =     {"mac-address":   "00:0a:0b:cc:0d:0e", "ifindex":   18, "vlan":   "100"}
-obj   =  cps_utils.CPSObject('base-mac/table',data=   d)
-tr_obj   =  ('create',   obj.get())
+# Register the attribute type
 
-transaction   =  cps_utils.CPSTransaction([tr_obj])
-ret   =  transaction.commit()
+cps_utils.add_attr_type('base-mac/table/mac-address', 'mac')
 
-if not   ret:
-    raise   RuntimeError   ("Error   creating   MAC   Table   Entry") 
+# Define the MAC address, interface index and VLAN attributes
 
+d = {'mac-address': '00:0a:0b:cc:0d:0e', 'ifindex': 18, 'vlan': '100'}
 
+# Create a CPS object
+
+obj = cps_utils.CPSObject('base-mac/table', data=d)
+
+# Associate the operation to the CPS object
+
+tr_obj = ('create', obj.get())
+
+# Create a transaction object
+
+transaction = cps_utils.CPSTransaction([tr_obj])
+
+# Check for failure
+
+ret = transaction.commit()
+
+if not ret:
+    raise RuntimeError('Error   creating   MAC   Table   Entry')
+    
+print 'Successfully created'
