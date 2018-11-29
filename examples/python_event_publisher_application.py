73b66ec21a397c89e77d3764063ed49819100b43
Amy Buck <amy_buck@dell.com>
2018-11-29 08:32:34 -0800

    Update python_event_publisher_application.py

--- examples/python_event_publisher_application.py
+++ examples/python_event_publisher_application.py
@@ -1,11 +1,10 @@
+#Python code block for CPS event publisher application
+
 import   cps
 import   cps_utils
 
-
 handle   =  cps.event_connect()
-
 
 obj   =  cps_utils.CPSObject('base-port/interface',qual='observed', data=   {"ifindex":23})
 
 cps.event_send(handle,   obj.get())
-
