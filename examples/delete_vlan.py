1ec0d5dcf0fdc4dc29940dcc44c2dda2717718f1
Amy Buck <amy_buck@dell.com>
2018-11-29 07:50:31 -0800

    Update delete_vlan.py

--- examples/delete_vlan.py
+++ examples/delete_vlan.py
@@ -1,30 +1,23 @@
-#!/usr/bin/python
 # Python code block to delete VLAN
 
 import cps
 import cps_object
 
-# Create CPS Object
-
+#Create CPS object
 cps_obj = \
     cps_object.CPSObject('dell-base-if-cmn/if/interfaces/interface')
 
-# Populate the Vlan attributes VLAN_ID='br100'
-
+#Populate the VLAN attributes VLAN_ID='br100'
 VLAN_ID = 'br100'
 cps_obj.add_attr('if/interfaces/interface/name', VLAN_ID)
 
-# Associate a CPS Set Operation with the CPS Object
-
+#Associate a CPS set operation with the CPS object
 cps_update = {'change': cps_obj.get(), 'operation': 'delete'}
 
-# Add the CPS operation,obj pair to a new CPS Transaction
-
+#Add the CPS operation,obj pair to a new CPS transaction
 transaction = cps.transaction([cps_update])
 
-# Check for failure
-
+#Check for failure
 if not transaction:
     raise RuntimeError('Error in deleting Vlan')
-
 print 'successful'
