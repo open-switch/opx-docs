159992fab33b5ec2c9081b569308bdf6846a9df6
Amy Buck <amy_buck@dell.com>
2018-11-29 07:51:26 -0800

    Update configure_ip_address.py

--- examples/configure_ip_address.py
+++ examples/configure_ip_address.py
@@ -1,34 +1,27 @@
-#!/usr/bin/python
-# Python code block to set IP address
+#Python code block to set IP address
 
 import cps_utils
 
-# Populate attributes for the CPS object
-
+#Populate attributes for the CPS object
 ifindex = 16
 ip_addr = '10.0.0.1'
 pfix_len = 16
 ip_attributes = {'base-ip/ipv4/ifindex': ifindex, 'ip': ip_addr,
                  'prefix-length': pfix_len}
 
-# Create CPS object
-
+#Create CPS object
 cps_utils.add_attr_type('base-ip/ipv4/address/ip', 'ipv4')
 cps_obj = cps_utils.CPSObject('base-ip/ipv4/address',
                               data=ip_attributes)
 
-# Create CPS transaction for object create
-
+#Create CPS transaction for object create
 cps_update = ('create', cps_obj.get())
 transaction = cps_utils.CPSTransaction([cps_update])
 
-# Commit transaction
-
+#Commit transaction
 ret = transaction.commit()
 
-# Check for failure
-
+#Check for failure
 if not ret:
     raise RuntimeError('Error   ')
-
 print 'Successfully created'
