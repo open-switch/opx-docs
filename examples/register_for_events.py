ad289bf67fab330f50994e5fb01fab069646ac0a
Amy Buck <amy_buck@dell.com>
2018-11-29 08:04:34 -0800

    Update register_for_events.py

--- examples/register_for_events.py
+++ examples/register_for_events.py
@@ -1,14 +1,11 @@
-#!/usr/bin/python
-# Python code to register for events
+#Python code block to register for events
 
 import cps
 
-# Create a handle to connect to the event service
-
+#Create a handle to connect to the event service
 handle = cps.event_connect()
 
-# Register a key with the event service to receive notification when an event is published
-
+#Register a key with the event service to receive notification when an event is published
 cps.event_register(handle, cps.key_from_name('observed',
                    'base-port/interface'))
 while True:
