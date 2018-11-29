93bd5b28a0be89e7aa9d48bd33e2ca3c0ba3efbf
Amy Buck <amy_buck@dell.com>
2018-11-29 08:15:22 -0800

    Create python_event_subscriber_application.py

---
+++ examples/python_event_subscriber_application.py
@@ -0,0 +1,14 @@
+#Python block code for CPS event subscriber application
+import cps
+import cps_utils
+
+handle = cps.event_connect()
+
+cps.event_register(handle, cps_api_object_key)
+
+while True:
+   ev = cps.event_wait(handle)
+
+   if ev[‘key’] == ...:
+       ...      elif ev['key'] == ...:
+       ...
