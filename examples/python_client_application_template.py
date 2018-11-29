8eecbb4e54e83833f53ac3e9d5c6f4844915e6f9
Amy Buck <amy_buck@dell.com>
2018-11-29 08:29:39 -0800

    Update python_client_application_template.py

--- examples/python_client_application_template.py
+++ examples/python_client_application_template.py
@@ -1,7 +1,9 @@
+#Python code block for CPS client application
+
 import   cps
 import   cps_utils
 
-#  Example   GET  request cps_get_response   =  []
+#Example get request cps_get_response = []
 cps.get([cps.key_from_name('observed','base-pas/chassis')], cps_get_response) 
 
 chassis_vendor_name   =  cps_attr_get(cps_get_response[0]['data'],'base-pas/chassis/vendor-name')
