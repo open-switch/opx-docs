a0fccf9dc0b046edc48c19919125bfe8f4f4bc5f
Amy Buck <amy_buck@dell.com>
2018-11-29 07:52:17 -0800

    Update delete_ip_address.py

--- examples/delete_ip_address.py
+++ examples/delete_ip_address.py
@@ -1,34 +1,27 @@
-#!/usr/bin/python
-# Python code block to delete IP address
+#Python code block to delete IP address
 
 import cps_utils
 
-# Populate attributes for the CPS object
-
+#Populate attributes for the CPS object
 idx = 16
 ip_addr = '10.0.0.1'
 pfix_len = 16
 ip_attributes = {'base-ip/ipv4/ifindex': idx, 'ip': ip_addr,
                  'prefix-length': pfix_len}
 
-# Create CPS object
-
+#Create CPS object
 cps_utils.add_attr_type('base-ip/ipv4/address/ip', 'ipv4')
 cps_obj = cps_utils.CPSObject('base-ip/ipv4/address',
                               data=ip_attributes)
 
-# Create CPS transaction to delete the CPS object
-
+#Create CPS transaction to delete the CPS object
 cps_update = ('delete', cps_obj.get())
 transaction = cps_utils.CPSTransaction([cps_update])
 
-# Commit the transaction
-
+#Commit the transaction
 ret = transaction.commit()
 
-# Check for failure
-
+#Check for failure
 if not ret:
     raise RuntimeError('Error   ')
-
 print 'Successfully deleted'
