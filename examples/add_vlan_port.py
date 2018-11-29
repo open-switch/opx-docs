6bd5c334e9b101f6fd57b4455eda02a2e9732055
Amy Buck <amy_buck@dell.com>
2018-11-29 07:47:56 -0800

    Update add_vlan_port.py

--- examples/add_vlan_port.py
+++ examples/add_vlan_port.py
@@ -1,36 +1,28 @@
-#!/usr/bin/python
-# Python   code block   to   add port   to VLAN
+#Python code block to add port to VLAN
 
 import cps
 import cps_object
 
-#  Create   CPS   Object
-
+#Create CPS object
 cps_obj = \
     cps_object.CPSObject('dell-base-if-cmn/if/interfaces/interface')
 
-#  Populate   the   Vlan   attributes VLAN_ID='br100'
-
+#Populate the VLAN attributes VLAN_ID='br100'
 VLAN_ID = 'br100'
 cps_obj.add_attr('if/interfaces/interface/name', VLAN_ID)
 
-#  Add   one or   more   ports   to   the   untagged-ports   property   of   the   VLAN
-
+#Add one or more ports to the untagged-ports property of the VLAN
 if_port_list = ['e101-001-0', 'e101-002-0', 'e101-003-0']
 cps_obj.add_attr('dell-if/if/interfaces/interface/untagged-ports',
                  if_port_list)
 
-#  Associate   a CPS   Set   Operation   with   the   CPS   Object
-
+#Associate a CPS set operation with the CPS object
 cps_update = {'change': cps_obj.get(), 'operation': 'set'}
 
-#  Add   the   CPS   operation,obj   pair   to   a new   CPS   Transaction
-
+#Add the CPS operation,obj pair to a new CPS transaction
 transaction = cps.transaction([cps_update])
 
-#  Check   for   failure
-
+#Check for failure
 if not transaction:
     raise RuntimeError('Error   in   adding   port   to   Vlan')
-
 print 'successful'
